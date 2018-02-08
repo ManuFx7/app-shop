88b44041b04738d9f428e1eaa0754642e08a3a9a
Manu Paez <manu9lin@gmail.com>
2018-02-08 08:43:14 +0100

    Proyecto Base

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Rutas de la Aplicación
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/','TestController@welcome');
+
+Route::get('/prueba', function(){
+	return 'Hola soy la ruta prueba';
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
